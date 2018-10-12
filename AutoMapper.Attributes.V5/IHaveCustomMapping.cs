53fbdfcc28ba29561b975987c4561255404564f5
AgentFire <agentfire@mail.ru>
2018-10-12 17:31:23 +0300

    Added IHaveCustomMapping.

---
+++ AutoMapper.Attributes.V5/IHaveCustomMapping.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoMapper.Attributes
+{
+    /// <summary>
+    /// This is placed on your derived <see cref="MapsFromAttribute"/> class.
+    /// </summary>
+    public interface IHaveCustomMapping<TSource, TTarget>
+    {
+        /// <summary>
+        /// Allows you to manually configure your maps.
+        /// </summary>
+        /// <param name="mappingExpression"></param>
+        void ConfigureMapping(IMappingExpression<TSource, TTarget> mappingExpression);
+    }
+}
